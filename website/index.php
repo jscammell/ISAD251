9ac10642099fd33e8dd5e6f442b78fa4ea146183
jscammell <44771964+jscammell@users.noreply.github.com>
2019-10-30 15:41:51 +0000

    Initial commit, header html and css started and index created

---
+++ website/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Home</title>
+	
+	        <link href="header.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
+	<div class="header"><?php include('./header.html');?></div>
+	
+	
+	
+	
+	<div class="header"><?php include('./footer.html');?></div>	
+</body>
+</html>
