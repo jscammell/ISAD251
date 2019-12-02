473c48c445161fb18327009f13958053ac351b59
jscammell <44771964+jscammell@users.noreply.github.com>
2019-12-02 11:51:47 +0000

    Delete Product_Query.sql

--- database/Product_Query.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Product(
-    productId INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
-    productName VARCHAR(40) NOT NULL,
-    productDetails VARCHAR(100) NOT NULL,
-    stockCount INT NOT NULL,
-    category VARCHAR(40) NOT NULL,
-    cost DECIMAL(4,2) NOT NULL,
-)
